8ad9e890582e4d02ba28b5b6a1c0e61f23c7e9bd
chidera-codes <chidera261@gmail.com>
2020-04-03 16:24:14 +0100

    user can now open an  account sucessfully

--- accounts.js
+++ accounts.js
@@ -24,27 +24,64 @@ open_account.addEventListener('click',function(){
   }
 });
 
-  // for(i = 0; i<=inputs; i++){
-  //   if(document.forms['my form'][i].value === ''){
-  //     console.log('empty input');
-  //     return false;
-  //   }
-  //   else{
-  //     return true;
-  //   }
-  // }
-
   form.addEventListener('submit' , (e)=>{
     e.preventDefault();
     var isValid = validation();
     if(isValid){
-      e.target.submit();  
+      function display_table(params) {
+        var table = document.getElementById("accounts_table");
+   
+        var row= document.createElement("tr");
+        var td1 = document.createElement("td");
+        var td2 = document.createElement("td");
+        var td3 = document.createElement("td");  
+        var td4 = document.createElement("td"); 
+        var td5 = document.createElement("td"); 
+        var td6 = document.createElement("td");
+        var today  = new Date(); 
+     
+        td1.innerHTML = today.toLocaleDateString("en-US");
+        td2.innerHTML  = Math.floor ('22'  + Math.random() * 100000000);
+        td3.innerHTML  = document.getElementById("input_account_type").value;
+        td4.innerHTML = 'Draft';
+        td5.innerHTML  = ' ₦' + document.getElementById("input_open_account").value + ".00";
+        td6.innerHTML  = ' ₦' + "0.00";
+
+     
+        row.appendChild(td1);
+        row.appendChild(td2);
+        row.appendChild(td3);
+        row.appendChild(td4);
+        row.appendChild(td5);
+        row.appendChild(td6);
+     
+        table.children[0].appendChild(row);
+
+      localStorage.setItem("Date", td1.innerText);
+      localStorage.setItem("Account number", td2.innerText);
+      localStorage.setItem("Account type", td3.innerText);
+      localStorage.setItem("Status", td4.innerText);
+      localStorage.setItem("Opening balance", td5.innerText);
+      localStorage.setItem("Current balance", td6.innerText);
+ 
+        var noacct_text = document.getElementById('noacct_text');
+        var table = document.getElementById('form');
+        var accounts_table = document.getElementById('accounts_table');
+        noacct_text.style.display = "none";
+        table.style.display = "none";
+        accounts_table.style.display = "block";
+        }
+        display_table();
+
+      // setTimeout(() => {
+      //   e.target.submit();
+        
+      // }, (5000 * 1000)); 
     }
     else{
     }
-  
-  
   });
+
 
 function validation() {
   var input_open_account = document.getElementById('input_open_account');
@@ -63,3 +100,4 @@ function validation() {
     return true;
   }
 } 
+
